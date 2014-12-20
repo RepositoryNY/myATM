b06f971b05c0bb49ece84c01ffd1730d5b1a324e
Lenivez <ruanaruy@gmail.com>
2014-12-20 19:47:41 +0200

    add fail to tests

--- src/main/java/ua/pti/myatm/MyATM.java
+++ src/main/java/ua/pti/myatm/MyATM.java
@@ -3,11 +3,18 @@ package ua.pti.myatm;
 public class MyATM {
 
     public static void main(String[] args) {
-//        double moneyInATM = 1000;
-//        ATM atm = new ATM(moneyInATM);
-//        Card card = null;
-//        atm.validateCard(card, 1234);
-//        atm.checkBalance();
-//        atm.getCash(999.99);        
+//        try
+//        {
+//            double moneyInATM = 1000;
+//            ATM atm = new ATM(moneyInATM);
+//            Card card = null;
+//            atm.validateCard(card, 1234);
+//            atm.checkBalance();
+//            atm.getCash(999.99);  
+//        }
+//        catch(Exception e)
+//        {
+//            e.printStackTrace();
+//        }
     }
 }
